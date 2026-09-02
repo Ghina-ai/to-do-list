d54099602a1b39fbdec1f3bcb7afb11582483bf2
Ghina <sistyaavinza@gmail.com>
2026-09-02 17:09:10 +0700

    update lagi 3

--- src/main.ts
+++ src/main.ts
@@ -145,6 +145,18 @@ clearAllBtn.addEventListener("click", () => {
   }
 });
 
+export function setupCounter(element: HTMLButtonElement) {
+  let counter = 0;
+
+  const setCounter = (count: number) => {
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+
+  element.addEventListener("click", () => setCounter(counter + 1));
+
+  setCounter(0);
+}
 
 loadTodos();
 render();
